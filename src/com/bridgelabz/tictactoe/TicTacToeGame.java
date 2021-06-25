3477576d44f4a79770460a82e35089e1b4c20a84
aravindkarri <aravindkarri98495@gmail.com>
2021-06-26 03:45:09 +0530

    ADDED: Method to check the winner of the tictactoe game

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -9,10 +9,21 @@ public class TicTacToeGame {
 		char userLetter = chooseUserLetter(userInput);
 		char computerLetter = (userLetter == 'X') ? 'O' : 'X';
 		char[] board = createBoard();
-		boolean userPlay = checkFirstPlayer();
+		checkFirstPlayer();
 		showBoard(board);
-		userPosition(userLetter, board);
-		checkFreeSpace(board);
+		while(true) {
+		playerTurn(userLetter, board);
+		if(winner(board,userLetter)) {
+			break;
+		}
+		showBoard(board);
+		computerTurn(computerLetter, board);
+		if (winner(board,computerLetter))
+		{
+			break;
+		}
+		showBoard(board);
+		}
 	}
 	private static char[] createBoard() {
 		char[] board = new char[10];
@@ -33,69 +44,82 @@ public class TicTacToeGame {
 		System.out.println("----------");
 		System.out.println( board[6] + " | " + board[7] + " | " + board[8] );
 	}
-	private static void userPosition(char userLetter, char[] board) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter your placement (1-9)");
-		int position = scanner.nextInt();
-		for(int i=0;i<=8;i++)
-		{
-			if(board[i]==' ')
+	private static void playerTurn(char userLetter, char[] board ) {
+		int userInput;
+		while(true) {			
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter your placement (1-9)");
+			userInput = scanner.nextInt();
+			if (isValidMove(board,userInput))
 			{
-				switch(position) {
+				break;
+			}
+			else
+			{
+				System.out.println(userInput + "is not a valid move");
+			}
+		}
+		placeMove(board,userInput, userLetter);
+	}
+	private static void placeMove(char[] board,int position,char symbol) {
+		switch(position) {
 				case 1:
-					board[0] = userLetter;
+					board[0] = symbol;
 					break;
 				case 2:
-					board[1] = userLetter;
+					board[1] = symbol;
 					break;
 				case 3:
-					board[2] = userLetter;
+					board[2] = symbol;
 					break;
 				case 4:
-					board[3] = userLetter;
+					board[3] = symbol;
 					break;
 				case 5:
-					board[4] = userLetter;
+					board[4] = symbol;
 					break;
 				case 6:
-					board[5] = userLetter;
+					board[5] = symbol;
 					break;
 				case 7:
-					board[6] = userLetter;
+					board[6] = symbol;
 					break;
 				case 8:
-					board[7] = userLetter;
+					board[7] = symbol;
 					break;
 				case 9:
-					board[8] = userLetter;
+					board[8] = symbol;
 					break;
 				}
-			}
-		}
-		showBoard(board);
 	}
-	private static void checkFreeSpace(char[] board) {
-		boolean freeSpace=false;
-		for(int index=0;index<board.length;index++)
-		{
-			if(board[index] == ' ')
-			{
-				freeSpace=true;
-			}
-		}
-		if(freeSpace == true)
-		{
-			System.out.println("Free space is available for the next move");
-		}
-		else
-		{
-			System.out.println("Free space is not available, Board is full");
+	private static boolean isValidMove(char[] board,int position) {
+
+		switch(position) {
+		case 1:
+			return board[0] == ' ';
+		case 2:
+			return board[1] == ' ';	
+		case 3:
+			return board[2] == ' ';
+		case 4:
+			return board[3] == ' ';
+		case 5:
+			return board[4] == ' ';
+		case 6:
+			return board[5] == ' ';
+		case 7:
+			return board[6] == ' ';
+		case 8:
+			return board[7] == ' ';
+		case 9:
+			return board[8] == ' ';
+		default:
+			return false;
 		}
 	}
 	private static boolean checkFirstPlayer() {
 		int Head=0;
 		boolean userPlay;
-		Scanner scanner = new Scanner(System.in);
 		double randomNum = Math.floor(Math.random()*10)%2;
 		if ( randomNum == Head )
 		{
@@ -108,6 +132,51 @@ public class TicTacToeGame {
 			userPlay=true;
 		}
 		return userPlay;
+	}
+	private static void computerTurn(char computerLetter, char[] board) {
+		int computerMove;
+		while(true)
+		{
+			computerMove = (int)Math.floor(Math.random()*10)%9 + 1;
+			if (isValidMove(board,computerMove)) {
+				break;
+			}
+			
+		}
+		System.out.println("Computer chose "+ computerMove);
+		placeMove(board,computerMove,computerLetter);
+	}
+	private static boolean winner(char[] board,char symbol)
+	{
+		if (( board[0] == symbol && board[1] == symbol && board[2] == symbol )|| 
+				(board[3] == symbol && board[4] == symbol && board[5] == symbol)||
+				(board[6] == symbol && board[7] == symbol && board[8] == symbol)|| 
+				(board[0] == symbol && board[4] == symbol && board[8] == symbol)||
+				(board[2] == symbol && board[4] == symbol && board[6] == symbol))
+		{
+			showBoard(board);
+			System.out.println("Player win the game");
+		}
+		else if (( board[0] == symbol && board[1] == symbol && board[2] == symbol )|| 
+				(board[3] == symbol && board[4] == symbol && board[5] == symbol)||
+				(board[6] == symbol && board[7] == symbol && board[8] == symbol)|| 
+				(board[0] == symbol && board[4] == symbol && board[8] == symbol)||
+				(board[2] == symbol && board[4] == symbol && board[6] == symbol))
+		{
+			showBoard(board);
+			System.out.println("Computer win the game");
+		}
+			
+			
+		for (int i=0;i<board.length;i++)
+		{
+			if (board[i] ==' ') {
+				return false;
+			}
+		}
+		showBoard(board);
+		System.out.println("The game ended in a tie!");
+		return true;
 	}
 }
 
